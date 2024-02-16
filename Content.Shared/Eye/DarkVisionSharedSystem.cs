8ee1a7e4ce467631a56c098f10d293d65cca3948
778b <kklokov@inbox.ru>
2024-02-16 20:04:50 +0400

    nightvision init

---
+++ Content.Shared/Eye/DarkVisionSharedSystem.cs
@@ -0,0 +1,28 @@
+using Robust.Shared.Network;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Eye;
+
+[Serializable, NetSerializable]
+public sealed class RequestUpdateOverlayMessage : EntityEventArgs
+{
+    public NetUserId Id { get; }
+    public String ShaderTexturePrototype { get; }
+    public Color LayerColor { get; }
+    public bool DrawLight { get; }
+    public bool On { get; }
+    public RequestUpdateOverlayMessage(NetUserId id, string shaderTexture,
+        Color layerColor, bool drawLight, bool toggleOn)
+    {
+        Id = id;
+        ShaderTexturePrototype = shaderTexture;
+        LayerColor = layerColor;
+        DrawLight = drawLight;
+        On = toggleOn;
+    }
+}
+
+public abstract class DarkVisionSharedSystem : EntitySystem
+{
+    // Maybe must be deleted
+}
