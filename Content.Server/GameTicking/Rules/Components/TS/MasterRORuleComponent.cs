3a67122193feac56542d7bced02ebc75bfa772dc
778b <kklokov@inbox.ru>
2024-02-24 18:04:33 +0400

    added rule ending

--- Content.Server/GameTicking/Rules/Components/TS/MasterRORuleComponent.cs
+++ Content.Server/GameTicking/Rules/Components/TS/MasterRORuleComponent.cs
@@ -1,10 +1,26 @@
 
 
+using Robust.Shared.Map;
+using System.Numerics;
+
 namespace Content.Server.GameTicking.Rules.Components;
 
 [RegisterComponent]
 public sealed partial class MasterRORuleComponent : Component
 {
+    public MapId? MissionMapId;
+    public MapId? MainStationMapId;
+    public Vector2 CurrentCenterMissionMap;
+    public EntityUid? MainStationUid;
+    public EntityUid? MainShuttleUid;
+
+    public bool IsAntagSpawned = false;
+
+    public List<EntityUid> SpawnedMissionEntities = new List<EntityUid>();
+
+    public WinMissionType WinType = WinMissionType.GarrisonMinorLose;
+
+    public List<WinMissionCondition> WinConditions = new();
 }
 
 
@@ -13,8 +29,6 @@ public enum WinMissionType : byte
     GarrisonMajorWin,
 
     GarrisonMinorWin,
-
-    Neutral,
 
     GarrisonMinorLose,
 
@@ -25,17 +39,17 @@ public enum WinMissionType : byte
 public enum WinMissionCondition : byte
 {
     AllAntagsDead,
-
     AllCrewDead,
-    NukeExplodedOnCorrectStation,
-    NukeExplodedOnNukieOutpost,
-    NukeExplodedOnIncorrectLocation,
-    NukeActiveInStation,
-    NukeActiveAtCentCom,
-    NukeDiskOnCentCom,
-    NukeDiskNotOnCentCom,
-    NukiesAbandoned,
-    AllNukiesDead,
-    SomeNukiesAlive,
-    AllNukiesAlive
+
+    SomeAntagsAlive,
+    SomeCrewAlive,
+
+    ShuttleLost,
+    AntagsOnMainStation,
+
+    SomeObjectivesComplete,
+    AllObjectivesComplete,
+
+    EvacuationShuttleCalled,
+    MainStationExplode
 }
--- Content.Server/GameTicking/Rules/TS/MasterRORuleSystem.cs
+++ Content.Server/GameTicking/Rules/TS/MasterRORuleSystem.cs
@@ -1,8 +1,16 @@
 using System.Linq;
 using System.Numerics;
+using Content.Server.Antag;
 using Content.Server.Chat.Systems;
 using Content.Server.GameTicking.Rules.Components;
+using Content.Server.NPC.Components;
+using Content.Server.NPC.Systems;
+using Content.Server.RoundEnd;
+using Content.Server.Spawners.EntitySystems;
+using Content.Server.Station.Components;
 using Content.Server.TS;
+using Content.Shared.Mind;
+using Content.Shared.Mobs.Systems;
 using Robust.Server.GameObjects;
 using Robust.Server.Maps;
 using Robust.Shared.Map;
@@ -19,6 +27,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
 {
     [Dependency] private readonly ChatSystem _chat = default!;
     [Dependency] private readonly MapSystem _mapSystem = default!;
+    [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
     [Dependency] private readonly MapLoaderSystem _mapLoader = default!;
     [Dependency] private readonly IMapManager _mapManager = default!;
     [Dependency] private readonly IEntityManager _entMan = default!;
@@ -27,12 +36,19 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
     [Dependency] private readonly ITimerManager _timerManager = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly ILocalizationManager _localizationManager = default!;
+    [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
+    [Dependency] private readonly RoundEndSystem _roundEndSystem = default!;
 
-    private MapId _currentMissionMapId;
-    private Vector2 _currentCenterMissionMap;
-    private List<EntityUid> _spawnedMissionEntities = new List<EntityUid>();
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<AntagCalculatedAndSpawnedEventArgs>(OnAntagsSpawned);
+    }
+
     protected override void Started(EntityUid uid, MasterRORuleComponent component, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
+        //base.Started(uid, component, gameRule, args);
         var smallestValue = 1;
         for (; smallestValue < 25; ++smallestValue)             // 25 is magic number
         {
@@ -46,7 +62,8 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
             _logManager.GetSawmill("RORule").Error("Event cant spawn map, because maps already more than 24");
             return;
         }
-        _currentMissionMapId = new MapId(smallestValue);
+        component.MissionMapId = new MapId(smallestValue);
+
 
         var allFoundMaps = _prototypeManager.EnumeratePrototypes<MissionMapPrototype>().ToList();
 
@@ -59,12 +76,12 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
         var allFoundGrids = _prototypeManager.EnumeratePrototypes<MissionGridPrototype>().ToList();
         var indexMap = _random.Next(allFoundMaps.Count() - 1);
 
-        if (!_mapLoader.TryLoad(_currentMissionMapId, allFoundMaps[indexMap].MapPath.ToString(), out var entityList))
+        if (!_mapLoader.TryLoad(component.MissionMapId.Value, allFoundMaps[indexMap].MapPath.ToString(), out var entityList))
         {
             return;
         }
 
-        var grids = _mapManager.GetAllMapGrids(_currentMissionMapId);
+        var grids = _mapManager.GetAllMapGrids(component.MissionMapId.Value);
         var mainGrid = grids.First();
 
         if (!grids.Any())
@@ -72,9 +89,9 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
             _logManager.GetSawmill("RORule").Error("Event cant found main grid!");
             return;
         }
-        var entityMap = _mapManager.GetMapEntityId(_currentMissionMapId);
+        var entityMap = _mapManager.GetMapEntityId(component.MissionMapId.Value);
 
-        _currentCenterMissionMap = _mapSystem.LocalToWorld(entityMap, mainGrid, Vector2.Zero);
+        component.CurrentCenterMissionMap = _mapSystem.LocalToWorld(entityMap, mainGrid, Vector2.Zero);
 
         int objectCount = _random.Next(5, 9);
         _logManager.GetSawmill("RORule").Info("Side grid count = {0}", objectCount);
@@ -90,12 +107,12 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
             tempOptions.Offset = new Vector2(
                 (startVector.X * Single.Cos(currentAngle) - startVector.Y * Single.Sin(currentAngle)),
                 (startVector.X * Single.Sin(currentAngle) + startVector.Y * Single.Cos(currentAngle))
-                ) * _random.NextFloat(140f, 220f) + _currentCenterMissionMap;
+                ) * _random.NextFloat(140f, 220f) + component.CurrentCenterMissionMap;
             tempOptions.Rotation = _random.NextAngle(0, 360);
 
-            _mapLoader.TryLoad(_currentMissionMapId, allFoundGrids[indexGrid].GridPath.ToString(), out _, tempOptions);
+            _mapLoader.TryLoad(component.MissionMapId.Value, allFoundGrids[indexGrid].GridPath.ToString(), out _, tempOptions);
         }
-        _mapManager.DoMapInitialize(_currentMissionMapId);
+        _mapManager.DoMapInitialize(component.MissionMapId.Value);
 
         var allSecretPools = _prototypeManager.EnumeratePrototypes<SecretPoolPrototype>().ToList();
 
@@ -112,7 +129,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
         {
             if (_entMan.TryGetComponent<TransformComponent>(tempEnt, out var tempTransform))
             {
-                if (tempTransform.MapID != _currentMissionMapId) continue;
+                if (tempTransform.MapID != component.MissionMapId.Value) continue;
             }
 
             if (_entMan.TryGetComponent<MetaDataComponent>(tempEnt, out var tempMeta))
@@ -136,7 +153,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
             _logManager.GetSawmill("RORule").Info("Found {0} mission item's spawners on RO map", missionItemSpawners.Count());
 
         // @todo antag player system balance
-        var guaranteedPointsCount = 3;
+        var guaranteedPointsCount = 1;
         var sidePointsCount = 2;
         int debugCountItems = 0;
 
@@ -154,7 +171,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
             {
                 foreach (var tempMissionItem in missionItemSpawners)
                 {
-                    if (spawnMissionItem(allSecretPools[tempPoolIndex], tempMissionItem, entityMap, mainGrid))
+                    if (spawnMissionItem(allSecretPools[tempPoolIndex], tempMissionItem, entityMap, mainGrid, component))
                         ++debugCountItems;
                 }
             }
@@ -165,7 +182,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
                     var tempRandMissionIndex = _random.Next(missionItemSpawners.Count());
 
                     if (spawnMissionItem(allSecretPools[tempPoolIndex], missionItemSpawners[tempRandMissionIndex],
-                            entityMap, mainGrid))
+                            entityMap, mainGrid, component))
                     {
                         missionItemSpawners.Remove(missionItemSpawners[tempRandMissionIndex]);
                         ++debugCountItems;
@@ -177,7 +194,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
                 {
                     foreach (var tempMissionItem in missionItemSpawners)
                     {
-                        if (spawnMissionItem(allSecretPools[tempPoolIndex], tempMissionItem, entityMap, mainGrid))
+                        if (spawnMissionItem(allSecretPools[tempPoolIndex], tempMissionItem, entityMap, mainGrid, component))
                             ++debugCountItems;
                     }
                 }
@@ -187,7 +204,7 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
                     {
                         var tempRandMissionIndex = _random.Next(missionItemSpawners.Count());
                         if (spawnMissionItem(allSecretPools[tempPoolIndex], missionItemSpawners[tempRandMissionIndex],
-                                entityMap, mainGrid))
+                                entityMap, mainGrid, component))
                         {
                             missionItemSpawners.Remove(missionItemSpawners[tempRandMissionIndex]);
                             ++debugCountItems;
@@ -205,26 +222,130 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
     }
 
 
-    protected override void ActiveTick(EntityUid uid, MasterRORuleComponent component, GameRuleComponent gameRule, float frameTime)
+    protected override void ActiveTick(EntityUid uid, MasterRORuleComponent component, GameRuleComponent gameRule,
+        float frameTime)
     {
-        //_chat.DispatchGlobalAnnouncement("Added gamerule", "SpecialForces", true, null, Color.BetterViolet);
+        base.ActiveTick(uid, component, gameRule, frameTime);
+
+        // fastest implementation without some functionality
+        // @todo refactor this
+
+        if (!component.IsAntagSpawned || component.MainStationUid == null || component.MainShuttleUid == null)
+            return;
+
+        MapId? mainMapId = null;
+        if (TryComp<TransformComponent>(component.MainStationUid, out var xformMain))
+        {
+            mainMapId = xformMain.MapID;
+        }
+
+        bool isAllItems = true;
+        bool isSomeItems = false;
+        foreach (var missionItem in component.SpawnedMissionEntities)
+        {
+            if (TryComp<TransformComponent>(missionItem, out var xform))
+            {
+                if (xform.MapID == mainMapId && mainMapId != null)
+                {
+                    isSomeItems = true;
+                }
+                else
+                {
+                    isAllItems = false;
+                }
+            }
+        }
+
+        bool isAllAntagDead = true;
+        bool isSomeAntagDead = false;
+        bool isSomeAntagOnMainMap = false;
+        var query = EntityQueryEnumerator<MissionAntagComponent, TransformComponent>();
+        while (query.MoveNext(out var eq_uid, out var eq_component, out var eq_xform))
+        {
+            if (_mobStateSystem.IsAlive(eq_uid))
+            {
+                if (eq_xform.MapID == mainMapId && mainMapId != null)
+                {
+                    isSomeAntagOnMainMap = true;
+                }
+
+                if (component.MainShuttleUid != null)
+                {
+                    if (eq_xform.GridUid == component.MainShuttleUid)
+                        isSomeAntagOnMainMap = true;
+                }
+
+                isAllAntagDead = false;
+            }
+            else
+            {
+                isSomeAntagDead = true;
+            }
+        }
+
+        if (isAllAntagDead && isAllItems)
+        {
+            component.WinConditions.Add(WinMissionCondition.AllAntagsDead);
+            component.WinConditions.Add(WinMissionCondition.AllObjectivesComplete);
+            component.WinType = WinMissionType.GarrisonMajorWin;
+            _roundEndSystem.EndRound();
+        }
     }
 
 
     private void startEvent()
     {
-        var ftlPoint = _entMan.SpawnEntity("FTLPointUnknown", new MapCoordinates(_currentCenterMissionMap, _currentMissionMapId));
-
+        var query = EntityQueryEnumerator<MasterRORuleComponent>();
+        while (query.MoveNext(out var uid, out var component))
+        {
+            if (component.MissionMapId == null) continue;
+            var ftlPoint = _entMan.SpawnEntity("FTLPointUnknown",
+                new MapCoordinates(component.CurrentCenterMissionMap, component.MissionMapId.Value));
+        }
         var senderLocale = _localizationManager.GetString("research-mission-sender");
         var messageLocale = _localizationManager.GetString("research-mission-message");
-
-
 
         _chat.DispatchGlobalAnnouncement(messageLocale, senderLocale, true, null, Color.GreenYellow);
     }
 
+    private void OnAntagsSpawned(AntagCalculatedAndSpawnedEventArgs args)
+    {
+        var query = EntityQueryEnumerator<MasterRORuleComponent>();
+        while (query.MoveNext(out var uid, out var component))
+        {
+            component.IsAntagSpawned = true;
 
-    private bool spawnMissionItem(SecretPoolPrototype secretPool, EntityUid missionItemSpawner, EntityUid entityMap, MapGridComponent mainGrid)
+            var stationQuery = EntityQueryEnumerator<TargetStationComponent>();
+            while (stationQuery.MoveNext(out var targetStation))
+            {
+                // @todo refactor for many stations
+                component.MainStationUid = targetStation.Owner;
+                break;
+            }
+
+            var shuttlesQuery = EntityQueryEnumerator<TargetShuttleComponent>();
+            while (shuttlesQuery.MoveNext(out var targetShuttle))
+            {
+                // @todo refactor for many stations
+                component.MainShuttleUid = targetShuttle.Owner;
+                break;
+            }
+
+            if (component.MainStationUid == null)
+            {
+                _logManager.GetSawmill("RORule").Error("Event cant found any target station!");
+            }
+
+            if (component.MainShuttleUid == null)
+            {
+                _logManager.GetSawmill("RORule").Error("Event cant found any target shuttle!");
+            }
+        }
+
+        _logManager.GetSawmill("RORule").Info("Shuttle and Station initialized in game rule!");
+    }
+
+    private bool spawnMissionItem(SecretPoolPrototype secretPool, EntityUid missionItemSpawner, EntityUid entityMap, MapGridComponent mainGrid, MasterRORuleComponent component)
     {
         if (!TryComp(missionItemSpawner, out TransformComponent? xComp))
         {
@@ -234,13 +355,16 @@ public sealed class MasterRORuleSystem : GameRuleSystem<MasterRORuleComponent>
             return false;
         }
 
+        if (component.MissionMapId == null)
+            return false;
+
         var tempItemPoolIndex = _random.Next(secretPool.PoolItems.Count() - 1);
         var spawnedItem = _entMan.Spawn(
             secretPool.PoolItems[tempItemPoolIndex],
             new MapCoordinates(_mapSystem.LocalToWorld(entityMap, mainGrid, xComp.LocalPosition),
-                _currentMissionMapId)
+                component.MissionMapId.Value)
         );
-        _spawnedMissionEntities.Add(spawnedItem);
+        component.SpawnedMissionEntities.Add(spawnedItem);
         return true;
     }
 }
